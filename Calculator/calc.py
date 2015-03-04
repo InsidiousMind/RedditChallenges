b9d0daa28ec6618c5510a293e1573230b762b6bf
Andrew Plaza <andrew.plaza15@gmail.com>
2015-03-04 12:35:45 -0500

    random commit

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -8,16 +8,17 @@ screen.keypad(1)
 
 screen.addstr("Welcome to the Insidious' Calculator!")
 
+screen.addstr("")
+
+
 while True:
     event = screen.getch()
     if event == ord("q"): break
     elif event == curses.KEY_UP:
         screen.clear()
-        screen.addstr("The User Pressed UP")
+        screen.addstr(5, 0, "The User Pressed UP")
     elif event == curses.KEY_DOWN:
-        screen.clear()
-        screen.addstr("The User Pressed DOWN")
-
+        screen.addstr(5, 0, "The User Pressed DOWN")
 
 
 curses.endwin()
